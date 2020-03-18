5f0fedd7987a41edaf8db3e673840f9ad136a81f
outerTape <42237490+outerTape@users.noreply.github.com>
2020-03-18 13:48:40 +0100

    Added addtitional operatos and a new constructor

--- Matrix/Main.cpp
+++ Matrix/Main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <vector>
 #include "Matrix.h"
 
 int main() {
 	
-	Matrix A = Matrix(10, 10);
-	Matrix B = Matrix(10, 10);
+	/*Matrix A = Matrix(2, 2);
+	Matrix B = Matrix(2, 2);
 
-	A.fill(1.3f);
-	B.fill(3.2f);
-	(A+B).print();
+	A.fill(2.0f);
+	B.fill(3.0f);
+	(A*B).print();*/
+	
+	std::vector<float> x{ 1.0f,2.0f };
+	std::vector<float> y;
+	y = x;
+	for (int i = 0; i < x.size(); i++) {
+		std::cout << x[i];
+	}
+	std::cout << std::endl;
 }
--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 struct Matrix {
     std::vector<float> matrix_values;
@@ -9,6 +10,12 @@ struct Matrix {
         n_rows = a;
         n_cols = b;
         matrix_values.assign(n_rows * n_cols, 0.0f);
+    }
+
+    Matrix(std::vector<float> values, int a, int b) {
+        n_rows = a;
+        n_cols = b;
+        assign(values);
     }
 
     void print() {
@@ -24,17 +31,55 @@ struct Matrix {
         matrix_values.assign(n_rows * n_cols, a);
     }
 
+    void assign(std::vector<float> values) {
+        matrix_values = values;
+    }
+
     float& operator()(int x, int y) {
-        return matrix_values[x + n_rows * y];
+        return matrix_values[y + n_cols * x];
     }
 
     Matrix operator+(Matrix& rhs) {
+        if ((n_rows != rhs.n_rows) | (n_cols != rhs.n_cols)) {
+            std::printf("Matrix dimensions are not equal!");
+            exit(-1);
+        }
+            
         Matrix sum(n_rows, n_cols);
-        for (int i = 0; i < n_rows; i++) {
-            for (int j = 0; j < n_cols; j++) {
-                sum(i, j) = (*this)(i, j) + rhs(i, j);
-            }
+        for (int i = 0; i < n_rows * n_cols; i++) {
+            sum.matrix_values[i] = matrix_values[i] + rhs.matrix_values[i];
         }
         return sum;
+    }
+
+    Matrix operator-(Matrix& rhs) {
+        if ((n_rows != rhs.n_rows) | (n_cols != rhs.n_cols)) {
+            std::printf("Matrix dimensions are not equal!");
+            exit(-1);
+        }
+
+        Matrix diff(n_rows, n_cols);
+        for (int i = 0; i < n_rows * n_cols; i++) {
+            diff.matrix_values[i] = matrix_values[i] - rhs.matrix_values[i];
+        }
+        return diff;
+    }
+
+    Matrix operator*(Matrix& rhs) {
+        if (n_cols != rhs.n_rows) {
+            std::printf("Matrix dimensions are compatible!");
+            exit(-1);
+        }
+
+        Matrix product(n_rows, rhs.n_cols);
+        for (int i = 0; i < n_rows; i++) {
+            for (int j = 0; j < rhs.n_cols; j++) {
+                for (int k = 0; k < n_cols; k++) {
+                    product(i, j) += (*this)(i, k) * rhs(k, j);
+                }
+            }
+            
+        }
+        return product;
     }
 };
